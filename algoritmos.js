c12a27a889b62c9ddcb7176dd396c85aa05d2e6a
nachoalaniz <nacho.alaniz13@gmail.com>
2022-07-22 11:02:18 -0300

    javaAlgoritmo

---
+++ algoritmos.js
@@ -0,0 +1,74 @@
+ var numero = 10 ;
+ var numero2 = 100 ;
+
+
+function resultado (){
+    let a = 8 ;
+    let b = 5 ;
+    const resta = a - b;
+    if (resta > 10){
+        return console.log ("El resultado es mayor que 12");
+    }
+    else {
+        return console.log ("el resultado es menor que 8");
+    }
+}
+resultado();
+
+function sumar (a,b){
+    const resultado = a + b ;
+    if (resultado > 18){
+        return console.log (`El resultado es ${resultado},mayor a 15`)
+    }
+    else {
+        return console.log (`el resultado es ${resultado}, menor a 10`);
+    }
+}
+sumar (5,4);
+
+function resta  (a,b){
+    const resultado = a - b ;
+    if (resultado > 4){
+        return console.log (`El resultado es ${resultado},mayor a 8`)
+    }
+    else {
+        return console.log (`el resultado es ${resultado}, menor a 6`);
+    }
+}
+resta (8,3);
+
+function multiplicacion  (a,b){
+    const resultado = a * b ;
+    if (resultado > 24){
+        return console.log (`El resultado es ${resultado},mayor a 23`)
+    }
+    else {
+        return console.log (`el resultado es ${resultado}, menor a 16`);
+    }
+}
+multiplicacion (8,3);
+
+function division (a,b){
+    const resultado = a / b ;
+    if (resultado > 30){
+        return console.log (`El resultado es ${resultado},mayor a 6`)
+    }
+    else {
+        return console.log (`el resultado es ${resultado}, menor a 4`);
+    }
+}
+division (150,5);
+
+function resultado (){
+    let a = 10 ;
+    let b = 8 ;
+    const resta = a - b;
+    if (resta > 4){
+        return console.log ("El resultado es mayor que 10");
+    }
+    else {
+        return console.log ("el resultado es menor que 8");
+    }
+}
+resultado();
+
